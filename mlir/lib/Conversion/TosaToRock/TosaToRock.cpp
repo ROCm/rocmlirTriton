f10da36517f1063b715011272d5a2949a3cdbebf
Justin Rosner <justin.rosner@amd.com>
2026-03-02 21:17:48 +0000

    Handle transposed GEMM output + tosa-to-rock-matmul LIT test

--- mlir/lib/Conversion/TosaToRock/TosaToRock.cpp
+++ mlir/lib/Conversion/TosaToRock/TosaToRock.cpp
@@ -1028,15 +1028,40 @@ public:
       setLastDims(nullptr, bScaleShape, {kDim, nDim});
       brBScale = insertBroadcast(scaleB, bScaleShape, loc, rw);
     }
+    // When transposeC is set, rock.gemm needs the un-transposed output type
+    // (G x M x N), since rock.gemm doesn't support output transpose.
+    RankedTensorType gemmOutputType = outputType;
+    if (transposeC) {
+      SmallVector<int64_t> gemmShape(outputType.getShape());
+      size_t rank = gemmShape.size();
+      std::swap(gemmShape[rank - 2], gemmShape[rank - 1]);
+      gemmOutputType =
+          RankedTensorType::get(gemmShape, outputType.getElementType());
+    }
+
     auto rockGemm = rock::GemmOp::create(
-        rw, loc, outputType, brA, brB, brAScale, brBScale, transposeA,
+        rw, loc, gemmOutputType, brA, brB, brAScale, brBScale, transposeA,
         transposeB, /*aScaleTransposed=*/nullptr, /*bScaleTransposed=*/nullptr,
         /*params=*/nullptr);
 
     if (auto attr = op->getAttrOfType<StringAttr>("perf_config"))
       rockGemm->setAttr("perf_config", attr);
 
-    rw.replaceOp(op, rockGemm.getResult());
+    Value result = rockGemm.getResult();
+
+    // If the output was transposed, insert a transpose after the gemm
+    // to produce the expected transposed shape.
+    if (transposeC) {
+      size_t rank = gemmOutputType.getRank();
+      SmallVector<int32_t> perms;
+      for (size_t i = 0; i < rank - 2; ++i)
+        perms.push_back(i);
+      perms.push_back(rank - 1);
+      perms.push_back(rank - 2);
+      result = rock::tosa::getTransposeOp(rw, loc, result, perms);
+    }
+
+    rw.replaceOp(op, result);
 
     return success();
   }
