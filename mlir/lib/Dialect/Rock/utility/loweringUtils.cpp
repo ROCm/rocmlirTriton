24ba9a1a8e468d1b81b949967b4b929a41333ad7
Pablo Antonio Martinez <pamartin@amd.com>
2026-03-06 10:06:47 +0100

    [AIROCMLIR-589] Fix fusability hang bug (#64)

--- mlir/lib/Dialect/Rock/utility/loweringUtils.cpp
+++ mlir/lib/Dialect/Rock/utility/loweringUtils.cpp
@@ -489,8 +489,8 @@ mlir::rock::traceRootOutputToStoreOps(Value output) {
         stores.insert(storeOp);
       } else if (auto reduceOp = dyn_cast<ReduceOp>(owner)) {
         worklist.push_back(reduceOp.getResult());
-      } else if (auto viewOp = dyn_cast_or_null<ViewLikeOpInterface>(owner)) {
-        worklist.push_back(viewOp.getViewSource());
+      } else if (auto viewOp = dyn_cast<ViewLikeOpInterface>(owner)) {
+        worklist.push_back(viewOp.getViewDest());
       } else if (isFusionOp(owner)) {
         for (Value result : owner->getResults()) {
           worklist.push_back(result);
