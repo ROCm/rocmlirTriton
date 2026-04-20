e486b4afd30b2361b27fc00e4dd3aa6c24fbb2e2
Pablo Antonio Martinez <pamartin@amd.com>
2026-04-20 12:28:40 +0000

    Fix API breakages in InitRocMLIRDialects

--- mlir/include/mlir/InitRocMLIRDialects.h
+++ mlir/include/mlir/InitRocMLIRDialects.h
@@ -240,7 +240,6 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::proton::gpu::registerConvertProtonNvidiaGPUToLLVM();
   mlir::triton::proton::gpu::registerConvertProtonAMDGPUToLLVM();
   mlir::triton::proton::gpu::registerAllocateProtonSharedMemoryPass();
-  mlir::triton::proton::gpu::registerAllocateProtonGlobalScratchBufferPass();
   mlir::triton::proton::gpu::registerScheduleBufferStorePass();
   mlir::triton::proton::gpu::registerAddSchedBarriersPass();
 
@@ -248,15 +247,10 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   if (std::string filename =
           mlir::triton::tools::getStrEnv("TRITON_PASS_PLUGIN_PATH");
       !filename.empty()) {
-
-    TritonPlugin TP(filename);
-    std::vector<const char *> passNames;
-    if (auto result = TP.getPassHandles(passNames); !result)
-      llvm::report_fatal_error(result.takeError());
-
-    for (const char *passName : passNames)
-      if (auto result = TP.registerPass(passName); !result)
-        llvm::report_fatal_error(result.takeError());
+    auto plugin = mlir::triton::plugin::TritonPlugin::load(filename);
+    if (!plugin)
+      llvm::report_fatal_error(plugin.takeError());
+    plugin->registerPasses();
   }
 
   registry.insert<
