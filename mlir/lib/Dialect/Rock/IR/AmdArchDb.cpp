2df5f1db4dd4c4c20915b1f6cf425d040bd42d6f
Pablo Antonio Martinez <pamartin@amd.com>
2026-04-27 09:56:34 +0000

    Remove break as they are uselss

--- mlir/lib/Dialect/Rock/IR/AmdArchDb.cpp
+++ mlir/lib/Dialect/Rock/IR/AmdArchDb.cpp
@@ -327,36 +327,26 @@ int64_t mlir::rock::getMinNumCU(StringRef arch) {
   switch (isaFamily) {
   case ISAFamily::GCN5_1:
     return 10;
-    break;
   case ISAFamily::CDNA1:
     return 120;
-    break;
   case ISAFamily::CDNA2:
     return 104;
-    break;
   case ISAFamily::CDNA3:
     return 20;
-    break;
   case ISAFamily::CDNA4:
     return 256;
-    break;
   case ISAFamily::RDNA1:
   case ISAFamily::RDNA2:
     return 30;
-    break;
   case ISAFamily::RDNA3:
     return 2;
-    break;
   case ISAFamily::RDNA4:
     return 12;
-    break;
   case ISAFamily::GFX1250:
     return 256;
-    break;
   default:
     return 1;
   }
-  return 1;
 }
 
 int64_t mlir::rock::getWaveSize(StringRef arch) {
