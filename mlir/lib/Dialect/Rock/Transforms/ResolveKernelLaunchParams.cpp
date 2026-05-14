fa629e5134a1d728bdb584c591bca3838f821c4f
Eric Zheng <Eric.Zheng@amd.com>
2026-05-14 13:21:41 +0000

    Revert exactly hit LDS fix

--- mlir/lib/Dialect/Rock/Transforms/ResolveKernelLaunchParams.cpp
+++ mlir/lib/Dialect/Rock/Transforms/ResolveKernelLaunchParams.cpp
@@ -92,7 +92,7 @@ struct ResolveKernelLaunchParamsPass
     }
 
     int64_t maxLDS = rock::getLDSSize(archStr);
-    if (sharedMemSize >= maxLDS) {
+    if (sharedMemSize > maxLDS) {
       rock::markAsNotApplicable(moduleOp);
       mlir::emitError(moduleOp.getLoc(), "ttg.shared (")
           << sharedMemSize << ") exceeds LDS limit (" << maxLDS << ") for "
