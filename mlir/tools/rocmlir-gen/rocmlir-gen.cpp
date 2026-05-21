1f1dca056436317411bcaccb4ac0dc84fef4a5a3
Daniel Hernandez-Juarez <dhernandez0@gmail.com>
2026-05-21 09:32:06 +0200

    [AIROCMLIR-792] Add gfx906 coverage and clean up unsupported-arch references (#226)

--- mlir/tools/rocmlir-gen/rocmlir-gen.cpp
+++ mlir/tools/rocmlir-gen/rocmlir-gen.cpp
@@ -118,14 +118,17 @@ static llvm::cl::opt<rock::KernelType> operation(
 
 static llvm::cl::opt<std::string> arch(
     "arch",
-    llvm::cl::desc("amdgpu architecture, eg: gfx803, gfx900, gfx906, gfx908"),
+    llvm::cl::desc("amdgpu architecture, eg: gfx906, gfx908, gfx942, gfx950, "
+                   "gfx1100, gfx1200, gfx1250"),
     llvm::cl::value_desc("GFX architecture string"), llvm::cl::init(""));
 
 static llvm::cl::opt<int> num_cu(
     "num_cu",
-    llvm::cl::desc("Number of compute units, valid combinations include: "
-                   "gfx803(36/64), gfx900(56/64), "
-                   "gfx906(60/64), gfx908(120)"),
+    llvm::cl::desc("Number of compute units. If omitted, defaults to the "
+                   "per-arch minimum returned by rock::getMinNumCU (e.g. "
+                   "gfx906=10, gfx908=120, gfx90a=104, gfx942=20, "
+                   "gfx950=256, gfx1010/gfx1030=30, gfx1100=2, gfx1200=12, "
+                   "gfx1250=256). Any positive value is accepted."),
     llvm::cl::value_desc("compute unit value"), llvm::cl::init(0));
 
 static llvm::cl::opt<int> numChiplets("num_chiplets",
