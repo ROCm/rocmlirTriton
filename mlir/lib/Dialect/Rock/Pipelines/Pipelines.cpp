3c5594412c1b0b4027d349b91c966073e59a5f28
Justin Rosner <justin.rosner@amd.com>
2026-07-10 13:44:30 +0000

    Remove Triton patch that reverted buffer soffset optimization series

--- mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
+++ mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
@@ -222,7 +222,8 @@ static void makeTTGIR(mlir::OpPassManager *pm, int threadPerWarp,
     pm->addPass(mlir::createTritonAMDGPUBlockPingpong({options.numStages}));
   }
 
-  if (isBufferOpsEnabled(options.useBufferOps)) {
+  bool useBufferOps = isBufferOpsEnabled(options.useBufferOps);
+  if (useBufferOps) {
     pm->addNestedPass<mlir::triton::FuncOp>(
         mlir::createTritonAMDGPUCanonicalizePointers());
     pm->addPass(mlir::createCanonicalizerPass());
@@ -239,6 +240,11 @@ static void makeTTGIR(mlir::OpPassManager *pm, int threadPerWarp,
       mlir::createTritonAMDGPUPrepareIfCombining());
   pm->addPass(mlir::createCanonicalizerPass());
   pm->addPass(mlir::createCSEPass());
+  if (useBufferOps) {
+    // Run after CSE so matching assume and loop-bound expressions share SSA,
+    // letting range analysis prove both non-negative.
+    pm->addPass(mlir::createTritonAMDGPUAnnotateBufferOpSplitSafety());
+  }
   pm->addPass(mlir::createSymbolDCEPass());
   // TODO(roctriton): Implement options like this.
   // if (options.instrumentationMode == "fpsan") {
