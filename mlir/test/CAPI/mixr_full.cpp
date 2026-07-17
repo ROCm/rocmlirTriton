a5e81227ed108ed544343b0d7b91521e0720c06d
Daniel Hernandez-Juarez <dhernandez0@gmail.com>
2026-07-17 10:44:14 +0200

    Improve tuning space for depthwise convs (#339)

--- mlir/test/CAPI/mixr_full.cpp
+++ mlir/test/CAPI/mixr_full.cpp
@@ -195,7 +195,7 @@ static bool constructAndTraverseIr(MlirContext ctx) {
       mlirRockTuningSpaceCreate(module, RocmlirTuningParamSetKindFull);
   printf("Got tuning space,\n");
   unsigned fNum = mlirRockTuningGetNumParams(tuningSpace);
-  // CHECK: full set = 720
+  // CHECK: full set = 540
   printf("full set = %u\n", fNum);
   MlirRockTuningParam tuningParam = mlirRockTuningParamCreate();
   MlirRockTuningTable tuningTable = mlirRockTuningTableCreate();
